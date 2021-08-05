4fbbc90cc7709a371f0dc3bb328df906cc58377b
Sandra De Brauwer <sandra.de.brauwer@student.ehb.be>
2021-08-05 11:56:30 +0200

    check box met toast

--- app/src/main/java/be/ehb/demoinput/MainActivity.java
+++ app/src/main/java/be/ehb/demoinput/MainActivity.java
@@ -7,7 +7,10 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -20,37 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btnConvert;
     private TextInputLayout layoutEuro;
     private TextInputLayout layoutDollar;
+    private CheckBox mCheckBox;
     // eventListener
-    private final View.OnClickListener convertOnClickListener=new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            if (!checkEuro()) {
-                return;
-            }
-           /*if(!checkDollar()){
-                return;
-            }*/
-            // drukken op button convert
-            // Waarde uit euro of dollar halen
-            String strEuro=etEuro.getText().toString();
-            // zet de waarde om naar double
-            // op de klasse en parse zet het om naar een primitief type
-            // value of zet om naar een object
-            double euro= Double.parseDouble(strEuro);
-
-            // omzetten naar Dollar;
-
-            // op de klasse van de String format daar komt een waarde van een variabele
-            double usd= euro * 1.18;
 
 
-            //proper
-            etDollar.setText(String.format(Locale.getDefault(),"%.2f",usd));
-            // werkvloer
-            // etDollar.setText(""usd);
-        }};
-
-   /*rivate boolean checkDollar() {String strDollar=etDollar.getText().toString();
+   /*private boolean checkDollar() {String strDollar=etDollar.getText().toString();
         if(strDollar.length()<=0){
         layoutDollar.setError("Field must be filled in");
         return false;}
@@ -86,7 +63,45 @@ public class MainActivity extends AppCompatActivity {
                 else {
             layoutEuro.setError(null); return true;
         }
+};
+    private CheckBox checkbox; private final View.OnClickListener convertOnClickListener=new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (!checkEuro()) {
+                return;
+            }
+           /*if(!checkDollar()){
+                return;
+            }*/
+            // drukken op button convert
+            // Waarde uit euro of dollar halen
+            String strEuro=etEuro.getText().toString();
+            // zet de waarde om naar double
+            // op de klasse en parse zet het om naar een primitief type
+            // value of zet om naar een object
+            double euro= Double.parseDouble(strEuro);
+
+            // omzetten naar Dollar;
+
+            // op de klasse van de String format daar komt een waarde van een variabele
+            double usd= euro * 1.18;
+
+
+            //proper
+            etDollar.setText(String.format(Locale.getDefault(),"%.2f",usd));
+            // werkvloer
+            // etDollar.setText(""usd);
+        }};
+// een comppundbutton is een combinatie is een button met stuk tekst- switch met label
+    private CompoundButton.OnCheckedChangeListener tosListener= new CompoundButton.OnCheckedChangeListener() {
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked){
+            Toast.makeText(getApplication(),"Check",Toast.LENGTH_SHORT).show();
+        }
 }
+};
+
 
     @Override
          protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +118,9 @@ public class MainActivity extends AppCompatActivity {
         layoutDollar=findViewById(R.id.til_dollar);
 
         etEuro.addTextChangedListener(textWatcherEuro);
+        checkbox=findViewById(R.id.ch_bx_turnOfservice);
+        checkbox.setOnCheckedChangeListener(tosListener);
+
 
     }
 }
